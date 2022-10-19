b94f904707e05f999b0dfae9a2d6bdaf96407309
Hamid <hamidskj123@gmail.com>
2022-10-19 15:27:13 +0700

    book

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\HeloController;
+use App\Http\Controllers\SiswaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::get('hallo', function(){  
+//     $data = ["me" => "hamid_ganteng"];
+//     return $data;
+// });
+
+// Route::get('hallo', [HeloController::class, 'index']);
+Route::resource('hallo', HeloController::class);
+Route::resource('siswa', SiswaController::class);
+Route::resource('book', BookController::class);
