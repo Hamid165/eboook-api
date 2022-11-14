23c6d13b6aea3f4fd0b220db034b3447f5479a47
Hamid <hamidskj123@gmail.com>
2022-11-14 08:40:00 +0700

    auth

--- routes/api.php
+++ routes/api.php
@@ -32,9 +32,9 @@ use App\Http\Controllers\BookController;
 // Route::resource('book', BookController::class);
 
 
-Route::middleware('auth:sanctum')->get('/user', function(Request $request){
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function(Request $request){
+//     return $request->user();
+// });
 
 // public route
 
